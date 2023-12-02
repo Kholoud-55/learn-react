2e345c96ab06eba287d86e75a1f60ccac78f22c8
Koloud <lolo3maari@gmail.com>
2023-12-02 21:03:40 +0300

    first commit

---
+++ src/components/memberInfo.js
@@ -0,0 +1,55 @@
+const teamProfiles = [
+  {
+    id: 1,
+    img: '/images/1.jpg',
+    name: 'Ethan Turner',
+    position: 'Software Engineer',
+    phone: '+1 555-1234',
+    email: 'ethan@example.com',
+  },
+  {
+    id: 2,
+    img: '/images/2.jpg',
+    name: 'Olivia Parker',
+    position: 'Project Manager',
+    phone: '+1 555-5678',
+    email: 'olivia@example.com',
+  },
+  {
+    id: 3,
+    img: '/images/3.jpg',
+    name: 'Mason Adams',
+    position: 'Graphic Designer',
+    phone: '+1 555-9876',
+    email: 'mason@example.com',
+    website: 'masonadams.design',
+  },
+  {
+    id: 4,
+    img: '/images/4.jpg',
+    name: 'Ava Foster',
+    position: 'Marketing Specialist',
+    phone: '+1 555-4321',
+    email: 'ava@example.com',
+  },
+  {
+    id: 5,
+    img: '/images/5.jpg',
+    name: 'Liam White',
+    position: 'CEO',
+    phone: '+1 555-8765',
+    email: 'liam@example.com',
+    website: 'liamwhite.com',
+  },
+  {
+    id: 6,
+    img: '/images/6.jpg',
+    name: 'Sophia Robinson',
+    position: 'Frontend Developer',
+    phone: '+1 555-3456',
+    email: 'sophia@example.com',
+    website: 'sophiarobinson.dev',
+  },
+];
+
+export default teamProfiles;
